e9a32de1c0cf43261e02f4c1012ef4e6ea713310
yuto-moriizumi <kurvan1112@gmail.com>
2022-04-30 20:02:38 +0900

    全画面UIを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <Container>
-      <p>test</p>
-      <Button>test2</Button>
+    <Container fluid className="h-100 bg-warning">
+      <Row className="h-100">
+        <Col className="bg-danger" xs={12}>
+          HEADER
+          <Button>test2</Button>
+        </Col>
+        <Col className="bg-danger align-self-end" xs={12}>
+          FOOTER
+        </Col>
+      </Row>
     </Container>
   );
 }
