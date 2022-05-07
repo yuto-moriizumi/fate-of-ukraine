cd22c75892aeedb253dd760f5c1dffd79417eaab
yuto-moriizumi <kurvan1112@gmail.com>
2022-05-07 22:00:53 +0900

    型をanyからViewportに変更

--- src/game/scene/SelectionScene.ts
+++ src/game/scene/SelectionScene.ts
@@ -1,9 +1,10 @@
+import { Viewport } from 'pixi-viewport';
 import * as PIXI from 'pixi.js';
 import { MapSprite } from '../container/MapSprite';
 import { Scene } from './Scene';
 
 export class SelectionScene extends Scene {
-  private map: any;
+  private map: Viewport;
   constructor() {
     super();
     const MAP_SRC = 'provinces.bmp';
