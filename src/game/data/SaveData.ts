72736b7af37665ff297cf97e7a439a5f220905f5
yuto-moriizumi <kurvan1112@gmail.com>
2022-05-07 20:17:22 +0900

    SaveDataにロード機能を追加

--- src/game/data/SaveData.ts
+++ src/game/data/SaveData.ts
@@ -1,9 +1,18 @@
-import { Country } from "./Country";
+import { Country } from './Country';
+import { Province } from './Provice';
 
 export class SaveData {
-    public countries!: Set<Country>;
+  public countries!: Set<Country>;
+  private _provinces!: Map<string, Province>;
 
-    constructor(json: object) {
-        Object.assign(this, json);
-    }
+  constructor(json: object) {
+    Object.assign(this, json);
+  }
+
+  private set provinces(provinces: { [key: string]: object }) {
+    Object.entries(provinces).forEach(([key, value]) =>
+      this._provinces.set(key, Object.assign(new Province(key), value))
+    );
+    console.log('gamedata provinces loaded:', this._provinces);
+  }
 }
