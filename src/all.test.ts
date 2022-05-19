11e4ec5ae7fcef8351da06c66bfb93f3baf15666
yuto-moriizumi <kurvan1112@gmail.com>
2022-05-20 00:15:06 +0900

    jestインポート修正　ダウンロード用テスト追加

--- src/all.test.ts
+++ src/all.test.ts
@@ -1,33 +1,48 @@
-import jest from 'jest';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { jest } from '@jest/globals';
 import { SaveData } from './game/data/SaveData';
-import { Test } from './game/Test';
 import { SAVEDATA_TYPE } from './game/type/JsonType';
-// import * as PIXI from 'pixi.js-legacy';
 
-test('fizzbuzzのユニットテスト', () => {
-  expect(Test.test(1)).toBe(2);
-});
+const testCountry = {
+  ABU: { name: 'Abu Dhabi', color: '#717060' },
+};
+const testCountries = { countries: { ...testCountry } };
+const testProvince = {
+  '#cce598': { name: 'Sitka', x: 711, y: 1992 },
+};
+const testProvinces = { provinces: { ...testProvince } };
 
 test('国データのロードができる', () => {
-  const data = new SaveData();
-  const testCountry = {
-    ABU: { name: 'Abu Dhabi', color: '#717060' },
-  };
-  data.loadJson({ countries: { ...testCountry } });
+  const data = new SaveData(testCountries);
   const countries = data.getCountries();
   expect(countries.get('ABU')).not.toBeNull();
   expect(countries.get('ABU')?.name).toBe(testCountry.ABU.name);
+  expect(data.toJson(SAVEDATA_TYPE.GAMEDATA).countries).toStrictEqual(
+    testCountry
+  );
 });
 
 test('プロヴィンスのロードができる', () => {
   const data = new SaveData();
-  const testProvince = {
-    '#cce598': { name: 'Sitka', x: 711, y: 1992 },
-  };
-  data.loadJson({ provinces: { ...testProvince } });
-  const countries = data.getProvinces();
-  expect(countries.get('#cce598')).not.toBeNull();
-  expect(countries.get('#cce598')?.name).toBe(testProvince['#cce598'].name);
+  data.loadJson(testProvinces);
+  const provinces = data.getProvinces();
+  expect(provinces.get('#cce598')).not.toBeNull();
+  expect(provinces.get('#cce598')?.name).toBe(testProvince['#cce598'].name);
   const output = data.toJson(SAVEDATA_TYPE.GAMEDATA);
   expect(output.provinces).toStrictEqual(testProvince);
+});
+
+describe('データのダウンロード', () => {
+  window.URL.createObjectURL = jest.fn();
+
+  afterEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window.URL.createObjectURL as any).mockReset();
+  });
+
+  test('データのダウンロード', () => {
+    new SaveData({ ...testCountries, ...testProvince }).download(
+      SAVEDATA_TYPE.GAMEDATA
+    );
+  });
 });
