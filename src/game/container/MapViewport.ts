814eae4a2fc2e64d52cd51c12e25f9c43d9c424c
volgakurvar <kurvan1112@gmail.com>
2023-07-20 00:22:29 +0900

    remap生成とダウンロードを追加

--- src/game/container/MapViewport.ts
+++ src/game/container/MapViewport.ts
@@ -27,9 +27,18 @@ export class MapViewport extends Viewport {
     this.provinceAtLeftClick = provinceRef;
 
     loader().load(MapViewport.MAP_SRC, (resource) => {
-      this.sprite = new PIXI.Sprite(resource.texture);
+      // this.sprite = new PIXI.Sprite(resource.texture);
+      const sprite = new PIXI.Sprite(resource.texture);
       const renderer = GameManager.instance.game.renderer;
-      this.spritePixelArray = renderer.plugins.extract.pixels(this.sprite);
+      this.spritePixelArray = renderer.plugins.extract.pixels(sprite);
+      this.sprite = new PIXI.Sprite(
+        PIXI.Texture.fromBuffer(
+          this.spritePixelArray,
+          sprite.width,
+          sprite.height
+        )
+      );
+      // this.sprite = new PIXI.Sprite(this.spritePixelArray);
       this.addChild(this.sprite);
       const { width, height } = renderer;
 
@@ -96,6 +105,7 @@ export class MapViewport extends Viewport {
     replacements: [Color, Color][],
     epsilon?: number
   ) {
+    return [];
     // return [new MultiColorReplaceFilter(replacements, epsilon ?? 0.005)];
     const CHUNK_SIZE = 500;
     const sliceCount = Math.ceil(replacements.length / CHUNK_SIZE);
@@ -131,6 +141,24 @@ export class MapViewport extends Viewport {
     const province = this.getProvinceByPoint(position);
     console.log('clicked point', position.x, position.y);
     if (!province) return; //プロヴィンスが存在しなければ何もしない
+
+    //試験的にクリックした箇所を塗る
+    this.fillColor(position);
+    // const idx = (position.x + position.y * this.sprite.width) * 4;
+    // console.log(idx);
+    // this.spritePixelArray[idx] = 255;
+    // this.spritePixelArray[idx + 1] = 0;
+    // this.spritePixelArray[idx + 2] = 0;
+    this.removeChild();
+    this.sprite = new PIXI.Sprite(
+      PIXI.Texture.fromBuffer(
+        this.spritePixelArray,
+        this.sprite.width,
+        this.sprite.height
+      )
+    );
+    this.addChild(this.sprite);
+
     // プロヴィンスの中心座標がおかしい場合は再計算
     if (
       province.x === 100 ||
@@ -157,6 +185,45 @@ export class MapViewport extends Viewport {
       provinces.set(provinceId, province);
     }
     return province;
+  }
+
+  private fillColor(point: Point) {
+    const provinceId = this.getProvinceIdFromPoint(point);
+    const province = data().provinces.get(provinceId);
+    if (!province) return undefined; //provinceがnullの時は何もしない
+
+    //BFSで探索
+    const candidates: Point[] = []; //{x: number, y: number,over:number} 形式
+    const already = new Set<number>();
+    candidates.push(point);
+    while (candidates.length > 0) {
+      const searchPoint = candidates.shift() as Point;
+      const idx =
+        (Math.floor(searchPoint.y) * this.sprite.width +
+          Math.floor(searchPoint.x)) *
+        4;
+      if (already.has(idx)) continue; //すでに探索済みだったら何もしない
+      already.add(idx);
+      const provinceId2 = this.getProvinceIdFromPoint(
+        new Point(searchPoint.x, searchPoint.y)
+      );
+      //黒線であれば超える
+      if (provinceId !== provinceId2) continue; // 色が異なる場合は何もしない
+      this.spritePixelArray[idx] = 255;
+      this.spritePixelArray[idx + 1] = 0;
+      this.spritePixelArray[idx + 2] = 0;
+
+      if (0 <= searchPoint.x - 1)
+        candidates.push(new Point(searchPoint.x - 1, searchPoint.y));
+      if (searchPoint.x + 1 < this.sprite.width)
+        candidates.push(new Point(searchPoint.x + 1, searchPoint.y));
+      if (0 <= searchPoint.y - 1)
+        candidates.push(new Point(searchPoint.x, searchPoint.y - 1));
+      if (searchPoint.y + 1 < this.sprite.height)
+        candidates.push(new Point(searchPoint.x, searchPoint.y + 1));
+    }
+
+    return;
   }
 
   // 指定した座標を含むプロヴィンスの中心座標を計算します
@@ -200,6 +267,86 @@ export class MapViewport extends Viewport {
     }
 
     return new Point(Math.floor(x / count), Math.floor(y / count));
+  }
+
+  public generateRemapTexture() {
+    // const image = new ImageData(this.sprite.width, this.sprite.height);
+
+    const remapArr = this.spritePixelArray.slice();
+
+    const main2remap = new Map<string, string>();
+    let idx = 0;
+
+    function toHexString(rgb: number[]) {
+      return PIXI.utils.hex2string(PIXI.utils.rgb2hex(rgb));
+    }
+    function string2rgb(str: string) {
+      return PIXI.utils.hex2rgb(PIXI.utils.string2hex(str));
+    }
+
+    for (let i = 0; i < remapArr.length; i += 4) {
+      const mainColor = Array.from(remapArr.slice(i, i + 3)).map(
+        (v) => v / 255
+      );
+      const key = toHexString(mainColor);
+      if (!main2remap.has(key)) {
+        const r = idx % 256;
+        const g = Math.floor(idx / 256);
+        const b = 0;
+        main2remap.set(key, toHexString([r / 255, g / 255, b / 255]));
+        idx++;
+      }
+      const remapColor = main2remap.get(key);
+      if (remapColor === undefined) {
+        new Error('remapColor was undefined');
+      }
+      const rgb = string2rgb(remapColor ?? '#ffff00');
+      remapArr[i] = Math.floor(rgb[0] * 255);
+      remapArr[i + 1] = Math.floor(rgb[1] * 255);
+      remapArr[i + 2] = Math.floor(rgb[2] * 255);
+    }
+
+    const sprite = new PIXI.Sprite(
+      PIXI.Texture.fromBuffer(remapArr, this.sprite.width, this.sprite.height)
+    );
+    this.addChild(sprite);
+
+    //ダウンロード
+
+    // const blob = new Blob([remapArr], { type: 'image/png' });
+    // const a = document.createElement('a');
+    // a.href = URL.createObjectURL(blob);
+    // a.download = 'remapTexture.png';
+    // a.click();
+
+    const canvas =
+      GameManager.instance.game.renderer.plugins.extract.canvas(sprite);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    canvas.toBlob((blob: any) => {
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      a.download = 'remapTexture.png';
+      a.click();
+    });
+
+    // const canvas = document.createElement('canvas');
+    // const ctx = canvas.getContext('2d');
+    // ctx?.putImageData(
+    //   new ImageData(remapArr, this.sprite.width, this.sprite.height),
+    //   0,
+    //   0
+    // );
+    // const imageData = ctx?.createImageData(
+    //   this.sprite.width,
+    //   this.sprite.height
+    // );
+    // imageData?.data.set(remapArr);
+
+    // const a = document.createElement('a');
+    // a.href = canvas.toDataURL('image/png', 1);
+    // a.download = 'image.jpg';
+    // a.click();
   }
 }
 
